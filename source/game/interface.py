5864ca05d848a8cf98c4867b123b3da0c1edc06d
v010ch <iakhremenko.v@gmail.com>
2025-09-03 01:34:41 +0300

    added command exchange and execution
    +worked variant of exchange between logic and interface
    +added exit command (exit_)
    +added command for new game (new__)
    +logic now execute recived commands

--- source/game/interface.py
+++ source/game/interface.py
@@ -18,7 +18,14 @@ class Game2048Interface(Frame):
     def __init__(self):
         Frame.__init__(self)
 
-        self.__GRID_SIZE = c.GRID_LEN
+        # establishing connection with server
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.connect((c.ADDR, c.PORT))
+        self.__GRID_SIZE = int(self.__socket.recv(1)
+                                            .decode()
+                               )
+        # self.__GRID_SIZE = c.GRID_LEN
+
         self.__command = 'idle'
         self.grid()
         self.master.title('2048')
@@ -37,17 +44,16 @@ class Game2048Interface(Frame):
             c.KEY_DOWN_ALT1: self.__down,
             c.KEY_LEFT_ALT1: self.__left,
             c.KEY_RIGHT_ALT1: self.__right,
+
+            c.KEY_QUIT: self.__exit,
         }
 
         self.grid_cells = []
         self.init_grid()
 
-        # establishing connection with server
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.connect((c.ADDR, c.PORT))
-
         self.__state = self.__get_new_state()
         self.__update_grid_cells()
+        print(self.__state)
 
 
     def __del__(self):
@@ -58,7 +64,7 @@ class Game2048Interface(Frame):
         '''
         '''
         background = Frame(self, bg=c.BG_COLOR_GAME,
-                           width=c.SIZE, height=c.SIZE,
+                           width=c.SIZE, height=c.SIZE, ## new sizes with new grid size
                            )
         background.grid()
 
@@ -112,13 +118,14 @@ class Game2048Interface(Frame):
 
 
     def __get_new_state(self, ) -> np.ndarray:
-        # get packet size
-        size = self.__socket.recv()
-        size = int(size.decode())
-        # get new state
-        tmp = self.__socket.recv(size)
+        '''
+        '''
+        print(f'getting {c.STATE_PACK_SIZE[self.__GRID_SIZE]}')
+        tmp = self.__socket.recv(c.STATE_PACK_SIZE[self.__GRID_SIZE])
+        tmp = np.frombuffer(tmp, dtype=np.uint16)\
+                .reshape((self.__GRID_SIZE, self.__GRID_SIZE))
 
-        return tmp.decode()
+        return tmp
 
 
     def key_up(self, event) -> None:
@@ -127,6 +134,7 @@ class Game2048Interface(Frame):
         key = event.keysym
         print(event)
         if key == c.KEY_QUIT:
+            done = self.commands[key]()
             exit()
 
         # if key == c.KEY_BACK and len(self.history_matrixs) > 1:
@@ -137,7 +145,10 @@ class Game2048Interface(Frame):
         if key in self.commands:
             # self.__state, done = self.commands[key]()
             done = self.commands[key]()
-            if done:
+            self.__update_grid_cells()
+
+            # if done:
+            if False:
                 # record last move
                 # self.history_matrixs.append(self.state)
                 self.__update_grid_cells()
@@ -220,127 +231,9 @@ class Game2048Interface(Frame):
 
         return True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class GameInterface(Frame):
-    '''
-    '''
-    def __init__(self):
-        Frame.__init__(self)
-
-        self.GRID_SIZE = c.GRID_LEN
-        self.grid()
-        self.master.title('2048')
-        # self.master.bind("<Key>", self.key_down)
-        self.master.bind("<KeyRelease>", self.key_up)
-
-        # set up methoda for key release event handler
-        self.commands = {
-            c.KEY_UP: logic.up,
-            c.KEY_DOWN: logic.down,
-            c.KEY_LEFT: logic.left,
-            c.KEY_RIGHT: logic.right,
-            c.KEY_UP_ALT1: logic.up,
-            c.KEY_DOWN_ALT1: logic.down,
-            c.KEY_LEFT_ALT1: logic.left,
-            c.KEY_RIGHT_ALT1: logic.right,
-        }
-
-        self.grid_cells = []
-        self.init_grid()
-        self.state = logic.new_game(self.GRID_SIZE)
-        self.history_matrixs = []
-        self.update_grid_cells()
-
-        # self.mainloop()
-
-    def init_grid(self) -> None:
+    def __exit(self) -> None:
         '''
+        Escape key event handler. End of the program.
         '''
-        background = Frame(self, bg=c.BG_COLOR_GAME,
-                           width=c.SIZE, height=c.SIZE,
-                           )
-        background.grid()
-
-        for i in range(self.GRID_SIZE):
-            grid_row = []
-            for j in range(self.GRID_SIZE):
-                cell = Frame(
-                    background,
-                    bg=c.BG_COLOR_CELL_EMPTY,
-                    width=c.SIZE / self.GRID_SIZE,
-                    height=c.SIZE / self.GRID_SIZE
-                )
-                cell.grid(
-                    row=i,
-                    column=j,
-                    padx=c.GRID_PADDING,
-                    pady=c.GRID_PADDING
-                )
-                t = Label(
-                    master=cell,
-                    text="",
-                    bg=c.BG_COLOR_CELL_EMPTY,
-                    justify=CENTER,
-                    font=c.FONT,
-                    width=5,
-                    height=2)
-                t.grid()
-                grid_row.append(t)
-            self.grid_cells.append(grid_row)
-
-    def update_grid_cells(self) -> None:
-        '''
-        '''
-        for i in range(self.GRID_SIZE):
-            for j in range(self.GRID_SIZE):
-                new_number = self.state[i][j]
-                if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=c.BG_COLOR_CELL_EMPTY)
-                else:
-                    self.grid_cells[i][j].configure(
-                        text=str(new_number),
-                        bg=c.BG_COLOR_DICT[new_number],
-                        fg=c.CELL_COLOR_DICT[new_number]
-                    )
-        self.update_idletasks()
-
-    def key_up(self, event) -> None:
-        '''
-        '''
-        key = event.keysym
-        print(event)
-        if key == c.KEY_QUIT:
-            exit()
-
-        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
-            self.state = self.history_matrixs.pop()
-            self.update_grid_cells()
-            print('back on step total step:', len(self.history_matrixs))
-        elif key in self.commands:
-            self.state, done = self.commands[key](self.state)
-            if done:
-                self.state = logic.add_new(self.state)
-                # record last move
-                self.history_matrixs.append(self.state)
-                self.update_grid_cells()
-                if logic.game_state(self.state) == 'win':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BG_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Win!", bg=c.BG_COLOR_CELL_EMPTY)
-                if logic.game_state(self.state) == 'lose':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BG_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Lose!", bg=c.BG_COLOR_CELL_EMPTY)
+        self.__command = 'exit_'
+        self.__send_command()
--- source/game/logic.py
+++ source/game/logic.py
@@ -20,39 +20,97 @@ class Game2048Logic():
     def __init__(self, size: Optional[int] = c.GRID_LEN):
         self.__weights = WEIGHTS2
         self.__cur_max = 2
+        self.__conn = None
 
         if size > 5 or size < 3:
-            raise ValueError(f'Wrong matrix size {size}.\nShould be >2 and <6.')
-        self.__size = size
-        self.__state = np.zeros((self.__size), dtype=np.uint16)
+            raise ValueError(f'Wrong size {size}.\nShould be 3 < and < 5.')
+
+        self.__grid_size = size
+
+        # create new game
+        self.__state = np.zeros((self.__grid_size, self.__grid_size),
+                                dtype=np.uint16
+                                )
+        self.__state = self.__new_game(self.__grid_size)
+
+        # command to exit main loop
         self.__exit = False
-        #self.server = socket.socket()
-        # self.server.bind((ADDR, PORT))
-        pass
+
 
     def run(self):
+        '''
+        Logic (server) main loop
+        '''
         print('run logic')
-        #self.server.bind(('localhost', 8089))
-        #self.server.listen(50)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            print('1')
             s.bind((c.ADDR, c.PORT))
             print('server logic await connection')
             s.listen(50)
-            conn, addr = s.accept()
+            self.__conn, addr = s.accept()
             print(f'Connection established with {addr}')
 
-            for idx in range(50):
-                print(f'{idx:02d}: this is logic')
-                time.sleep(0.5)
+            # send grid size and init state to interface
+            self.__conn.send(str(self.__grid_size).encode())
+            self.__send_state()
+
+            # for idx in range(50):
+            #     print(f'{idx:02d}: this is logic')
+            #     time.sleep(0.5)
 
             while not self.__exit:
-                # send state
                 # get comand
+                command = self.__conn.recv(c.CMD_LEN)
                 # calculate new state
-                self.__exit = True
+                done = self.__execute_command(command.decode())
+                if done:
+                    self.__state = self.__add_new(self.__state)
+                self.__send_state()
+                # self.__exit = True
+            print('logic: end of the main loop')
 
-    def new_game(self, n: int) -> np.ndarray:
+
+    def __send_state(self) -> None:
+        '''
+        Send state to consumer (interface/solver)
+        '''
+        print(self.__state)
+        tmp = self.__state.tostring()
+        print(f'sending {len(tmp)}')
+        self.__conn.send(tmp)
+
+
+    def __execute_command(self, cmd: str) -> None:
+        '''
+        Execute recived command/
+        '''
+        if cmd == 'up___':
+            self.__state, done = self.__up(self.__state)
+            return done
+
+        if cmd == 'down_':
+            self.__state, done = self.__down(self.__state)
+            return done
+
+        if cmd == 'left_':
+            self.__state, done = self.__left(self.__state)
+            return done
+
+        if cmd == 'right':
+            self.__state, done = self.__right(self.__state)
+            return done
+
+        if cmd == 'new__':
+            self.__state = self.__new_game(self.__grid_size)
+            return True
+
+        if cmd == 'exit_':
+            self.__exit = True
+            return True
+
+        return -1
+
+
+    def __new_game(self, n: int) -> np.ndarray:
         '''
         Init new game
 
@@ -60,19 +118,20 @@ class Game2048Logic():
             n: int - number of cells at row/columns
 
         return:
-            np.ndarray - empty (zero) game state matrix
+            np.ndarray - new game state matrix with only two init values
         '''
         self.__weights = WEIGHTS2
         self.__cur_max = 2
 
         state = np.zeros((n, n), dtype=np.uint16)  # ?uint8?
 
-        state = self.add_new(state)
-        state = self.add_new(state)
+        state = self.__add_new(state)
+        state = self.__add_new(state)
 
         return state
 
-    def add_new(self, state: np.ndarray) -> np.ndarray:
+
+    def __add_new(self, state: np.ndarray) -> np.ndarray:
         '''
         Add one new element (2, 4, or 8) to game state matrix.
         Weights will be changed when max value becomes 4 or 8.
@@ -90,21 +149,21 @@ class Game2048Logic():
             a = random.randint(0, state.shape[0] - 1)
             b = random.randint(0, state.shape[0] - 1)
 
-        element = random.choices(NEW_VALS, self.__weights)[0]
+        element = random.choices(NEW_VALS, self.__weights, k=1)[0]
         state[a, b] = element
 
         return state
 
 
-    def cover_up(self, state: np.ndarray):
+    def __cover_up(self, state: np.ndarray):
         '''
         '''
-        new = np.zeros((self.__size, self.__size), dtype=np.uint16)
-
+        new = np.zeros_like(state)
         done = False
-        for i in range(self.__size):
+
+        for i in range(self.__grid_size):
             count = 0
-            for j in range(self.__size):
+            for j in range(self.__grid_size):
                 if state[i, j] != 0:
                     new[i, count] = state[i, j]
                     if j != count:
@@ -114,9 +173,10 @@ class Game2048Logic():
         return new, done
 
 
-    def merge(self, state: np.ndarray, done) -> tuple[np.ndarray, bool]:
+    def __merge(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
         '''
-        Merge cells if possible
+        Merge cells if possible - return state and True.
+        If no cels was merged - return state and False.
 
         args:
             mat: np.ndarray - game state matrix
@@ -124,11 +184,12 @@ class Game2048Logic():
         return:
             Tuple:
                 np.ndarray - game state matrix
-                bool
+                bool - True if at least one merge (game not end)
         '''
-        for i in range(self.__size):
-            for j in range(self.__size-1):
-                if state[i, j] == state[i, j+1] and state[i, j] != 0:
+        done = False
+        for i in range(self.__grid_size):
+            for j in range(self.__grid_size - 1):
+                if state[i, j] == state[i, j + 1] and state[i, j] != 0:
                     state[i, j] *= 2
                     state[i, j+1] = 0
                     done = True
@@ -136,7 +197,7 @@ class Game2048Logic():
         return state, done
 
 
-    def up(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
+    def __up(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
         '''
         Up key event handler. Shift values up and merge them if possible.
 
@@ -145,18 +206,19 @@ class Game2048Logic():
 
         return:
             np.ndarray - new game state
+            bool - True - if game not ended
         '''
-        # print("up")
+        # print('logic: up')
         state = np.transpose(state)
-        state, done = self.cover_up(state)
-        state, done = self.merge(state, done)
-        state = self.cover_up(state)[0]
+        state, done_c = self.__cover_up(state)
+        state, done_m = self.__merge(state)
+        state = self.__cover_up(state)[0]
         state = np.transpose(state)
 
-        return state, done
+        return state, (done_c and done_m)
 
 
-    def down(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
+    def __down(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
         '''
         Down key event handler. Shift values down and merge them if possible.
 
@@ -165,18 +227,19 @@ class Game2048Logic():
 
         return:
             np.ndarray - new game state
+            bool - True - if game not ended
         '''
-        # print("down")
+        # print('logic: down')
         state = np.flip(np.transpose(state), axis=1)
-        state, done = self.cover_up(state)
-        state, done = self.merge(state, done)
-        state = self.cover_up(state)[0]
+        state, done_c = self.__cover_up(state)
+        state, done_m = self.__merge(state)
+        state = self.__cover_up(state)[0]
         state = np.transpose(np.flip(state, axis=1))
 
-        return state, done
+        return state, (done_c and done_m)
 
 
-    def left(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
+    def __left(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
         '''
         Left key event handler. Shift values left and merge them if possible.
 
@@ -185,16 +248,17 @@ class Game2048Logic():
 
         return:
             np.ndarray - new game state
+            bool - True - if game not ended
         '''
-        # print("left")
-        state, done = self.cover_up(state)
-        state, done = self.merge(state, done)
-        state = self.cover_up(state)[0]
+        # print('logic: left')
+        state, done_c = self.__cover_up(state)
+        state, done_m = self.__merge(state)
+        state = self.__cover_up(state)[0]
 
-        return state, done
+        return state, (done_c and done_m)
 
 
-    def right(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
+    def __right(self, state: np.ndarray) -> tuple[np.ndarray, bool]:
         '''
         Right key event handler. Shift values right and merge them if possible.
 
@@ -203,12 +267,13 @@ class Game2048Logic():
 
         return:
             np.ndarray - new game state
+            bool - True - if game not ended
         '''
-        # print("right")
+        # print('logic: right')
         state = np.flip(state, axis=1)
-        state, done = self.cover_up(state)
-        state, done = self.merge(state, done)
-        state = self.cover_up(state)[0]
+        state, done_c = self.__cover_up(state)
+        state, done_m = self.__merge(state)
+        state = self.__cover_up(state)[0]
         state = np.flip(state, axis=1)
 
-        return state, done
+        return state, (done_c and done_m)
